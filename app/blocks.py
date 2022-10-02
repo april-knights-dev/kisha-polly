ae008d4aa94b5083d205e5b2bacb41c3dc757949
神代浩歌 <khiroka@khiroka.local>
2022-10-02 09:26:55 +0900

    Change: vote_blocks.jsonの相対パス修正

--- app/blocks.py
+++ app/blocks.py
@@ -9,7 +9,7 @@ class BlocksChangeMessage:
     def __init__(self) -> None:
         try:
             # BOM有りファイルを読み込む時はutf-8-sigをつけると読み込める
-            with open("app/vote_blocks.json", "r", encoding="utf-8-sig") as jsonf:
+            with open("vote_blocks.json", "r", encoding="utf-8-sig") as jsonf:
                 self.blocks_format = json.load(jsonf)
         except FileNotFoundError:
             logging.error("vote_blocks.jsonが見つかりませんでした")
